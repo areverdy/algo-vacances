61ccf9b9938e2d2e9cdec53462e7d2b520a3cdca
areverdy <alec.rev1234@gmail.com>
2023-09-01 16:57:11 +0200

    ajout exercices algo

--- dist/index.js
+++ dist/index.js
@@ -30,4 +30,39 @@ function multiple(number) {
         }
     }
 }
-multiple(60);
+// multiple(30)
+function trouverjour(jour, mois, annee) {
+    const joursemaine = [
+        'dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'
+    ];
+    let z = 0;
+    let D = 0;
+    if (mois >= 3) {
+        z = annee;
+        D = ((Math.trunc(23 * mois) / 9) + jour + 4 + annee + (Math.trunc(z / 4)) - (Math.trunc(z / 100)) + (Math.trunc(z / 400)) - 2) % 7;
+    }
+    else {
+        z = annee - 1;
+        D = ((Math.trunc(23 * mois) / 9) + jour + 4 + annee + (Math.trunc(z / 4)) - (Math.trunc(z / 100)) + (Math.trunc(z / 400))) % 7;
+    }
+    console.log(Math.trunc(D));
+    console.log(joursemaine[Math.trunc(D)]);
+}
+trouverjour(18, 2, 2023);
+let entier1 = 10;
+let entier2 = 20;
+let entier3 = 30;
+function trouverPlusGrand(entier1, entier2, entier3) {
+    if (entier1 > entier2 && entier1 > entier3) {
+        return (entier1);
+    }
+    else if (entier2 > entier3 && entier2 > entier1) {
+        return (entier2);
+    }
+    else if (entier3 > entier2 && entier3 > entier1) {
+        return (entier3);
+    }
+    return (0);
+}
+const plusGrand = trouverPlusGrand(entier1, entier2, entier3);
+console.log("Le plus grand entier est : " + plusGrand);
